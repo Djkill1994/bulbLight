216867c92cd96418cb6794b954792c336f040303
Bruyok Vlad <djkill911@gmail.com>
2021-11-06 14:15:38 +0300

    Redesigned the project from the beginning, added a CSS light bulb and a button

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ function onBulb() {
     if (BULB_LIGHT.style.boxShadow === 'none') {
         BULB_LIGHT.style = 'box-shadow: 0 0 70px 80px white';
         BULB_LIGHT.style.background = 'white';
+        BUTTON_LIGHT.style =  'box-shadow: 0 0 20px 12px rgba(80, 135, 230, 0.9)';
     } else {
         BULB_LIGHT.style = 'box-shadow: none';
+        BUTTON_LIGHT.style = 'box-shadow: none';
+    }
+    if (BUTTON_LIGHT.style.boxShadow === 'none'){
+        BUTTON_LIGHT.style = 'box-shadow: block';
     }
 }
 
